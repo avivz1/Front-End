64904af33853c94b300afb9c86e8c3afe7b14334
avivz1 <avivzamir123@gmail.com>
2022-04-07 20:03:29 +0300

    Student - more details

--- Components/PracticesComponents/PracticeDetailsComponent.js
+++ Components/PracticesComponents/PracticeDetailsComponent.js
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View, Button, TextInput, ViewComponent, Platform, Alert } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, ViewComponent, Platform, Alert } from 'react-native';
 import React, { useState, useEffect } from 'react'
 import { Context } from '../../ContextAPI/Context';
 import axios from 'axios';
 import { IP } from '../../IP_Address';
-import { Picker } from '@react-native-picker/picker';
+import { DataTable } from 'react-native-paper'
+
 
 
 
@@ -11,59 +12,80 @@ import { Picker } from '@react-native-picker/picker';
 
 export default function PracticeDetailsComponent(props) {
 
-    const { teamsMap,userId } = React.useContext(Context);
+    const { teamsMap, userId } = React.useContext(Context);
     const [teamsNameMap, setMap] = teamsMap
-    const [precentage,setPrecentage] = useState('')
+    const [precentage, setPrecentage] = useState('')
     const [userIdValue] = userId;
-    const [index, setIndex] = React.useState(0);
+    const [studentsList,setStudentsList] = useState([])
 
 
 
 
-    useEffect(() => {
-        console.log(props.practice.Students)
+    useEffect(()=> {
+        getAttendancePrecent()
+        getStudentsFullDetails()
         let x = props.practice.Date.split('T')
         props.practice.Date = x[0];
-        getAttendancePrecent()
     }, []);
+
+    useEffect(()=>{
+
+    },[])
 
     const getTeamName = (teamId) => {
         let name = teamsNameMap.get(teamId);
         return name;
     }
-    
-    const onChangeIndex = (index) => {
-        setIndex(index)
+
+    const getStudentsFullDetails = () => {
+        let stuArr = props.practice.Students.filter(s=>s.Name==null)
+        axios.post('http://' + IP + '/students/getFewStudents', { students: stuArr }).then(res => {
+            if (res.data != false) {
+                setStudentsList(res.data)
+            }
+        })
     }
 
     const getAttendancePrecent = () => {
-        axios.post('http://' + IP + '/practices/attendancepercentage', { practice: props.practice ,userId:userIdValue}).then(res => {
-        if(res.data!=false){
-            setPrecentage(res.data)
-        }
+        axios.post('http://' + IP + '/practices/attendancepercentage', { practice: props.practice, userId: userIdValue }).then(res => {
+            if (res.data != false) {
+                setPrecentage(res.data)
+            }
         })
-
     }
-//{JSON.stringify(props.practice.Students)}
+    
     return (
 
-        <View >
-            <Text>Name : {props.practice.Name}</Text>
-            <Text>Date : {props.practice.Date}</Text>
-            <Text>Hour : {props.practice.PracticeHour}</Text>
-            <Text>Students :</Text>
-            <Picker
-                style={{ width: 100 }}
-                selectedValue={props.practice.Students[index].Name}
-                onValueChange={onChangeIndex}>
-                {props.practice.Students.map((student1, index) => {
-                    return (<Picker.Item key={index} label={student1._id} value={student1._id}></Picker.Item>)
-                })
-                }
+        <View style={[styles.container]}>
+            <Text style={{padding:5}} >Name : {props.practice.Name}</Text>
+            <Text style={{padding:5}}>Date : {props.practice.Date}</Text>
+            <Text style={{padding:5}}>Hour : {props.practice.PracticeHour}</Text>
+            <Text style={{padding:5}}>Total Students :{props.practice.Students.length}</Text>
 
-            </Picker>
-            <Text>Team : {getTeamName(props.practice.Team.Team_ID)}  </Text>
-            <Text>Arrivel Precent : {precentage}%  </Text>
+            <Text style={{padding:5}}>Team : {getTeamName(props.practice.Team.Team_ID)}  </Text>
+            <Text style={{padding:5}}>Arrivel Precent : {precentage ? precentage.toFixed(2) : ''}%  </Text>
+
+            <DataTable style={[styles.TableText]} >
+                <DataTable.Header>
+                    <DataTable.Title>Name</DataTable.Title>
+                    <DataTable.Title>Belt</DataTable.Title>
+                </DataTable.Header>
+            
+                {studentsList.length > 0 ? studentsList.map((stu, index) => {
+                    return (
+                        <DataTable.Row style={[styles.editForm]} key={index}>
+                                <DataTable.Cell>{stu.Name}</DataTable.Cell>
+                                <DataTable.Cell>{stu.Belt}</DataTable.Cell>
+                            </DataTable.Row>
+                        )
+                    })
+                    
+                    :
+                    <Text>No Students</Text>
+                    
+                }
+            </DataTable>
+
         </View>
     )
 }
@@ -71,14 +93,22 @@ export default function PracticeDetailsComponent(props) {
 
 
 const styles = StyleSheet.create({
+
+    head: { height: 400, backgroundColor: 'orange' },
+    wrapper: { flexDirection: 'row' },
+    title: { flex: 1, backgroundColor: '#2ecc71' },
+    row: { height: 28 },
+    text: { textAlign: 'center' },
+
+
     container: {
-        alignSelf: 'center',
-        flex: 1,
-        height: '40%',
-        width: '100%',
-        backgroundColor: '#fff',
-        fontSize: 10,
-    },
+        flex: 0,
+        padding: 10,
+        paddingTop: 10,
+        backgroundColor: '#ffffff',
+        height:'60%'
+      },
+      
     input: {
         margin: 15,
         height: 40,
@@ -90,6 +120,18 @@ const styles = StyleSheet.create({
         margin: 10,
         alignItems: 'center',
         justifyContent: 'center',
-    }
+    },
+    HeadStyle: {
+        height: 50,
+        alignContent: "center",
+        backgroundColor: '#CE39FE'
+      },
+      TableText: {
+        margin: 8,
+        height: 49,
+        padding:5,
+        alignContent: "center",
+        alignItems: 'center',
+      }
 });
 
--- Components/StudentsComponent/StudentDetailsComponent.js
+++ Components/StudentsComponent/StudentDetailsComponent.js
@@ -1,20 +1,52 @@
 import { StyleSheet, Text, View, Button, TextInput, ViewComponent, Platform, Alert } from 'react-native';
 import React, { useState, useEffect } from 'react'
 import { Context } from '../../ContextAPI/Context';
+import axios from 'axios';
+import { IP } from '../../IP_Address';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars'
 
 
 
 export default function ViewStudentComponent(props) {
     const { teamsMap } = React.useContext(Context);
     const [teamsNameMap, setMap] = teamsMap
+    const [practicesList, setPractices] = useState([])
+    const [okDates, setOkDates] = useState({})
 
     useEffect(() => {
+        getStudentPracticesDetails()
+    }, []);
 
-    });
+    const getStudentPracticesDetails = () => {
+        axios.post('http://' + IP + '/practices/getFewPractices', { practices: props.student.Practices }).then(res => {
+            if (res.data != false) {
+                let datesArr = []
+                setPractices(res.data)
+                res.data.forEach(pr => {
+                    let d = pr.Date.split('T')
+                    d = d[0]
+                    //d = d.toString().replace(/-/g, '/')
+                    datesArr.push(d)
+                });
+                processOkDates(datesArr)
+
+            }
+        })
+    }
 
     const getTeamName = (teamId) => {
         let name = teamsNameMap.get(teamId);
         return name;
+    }
+
+    const processOkDates = (arr) => {
+        let obj = {}
+        // {'2022-04-07': {selected: true, marked: true, selectedColor: 'blue'}
+        arr.forEach(date => {
+            obj[date] = { selected: true, marked: true }
+            return obj;
+        });
+        setOkDates(obj)
     }
 
     return (
@@ -24,6 +56,11 @@ export default function ViewStudentComponent(props) {
             <Text>Belt : {props.student.Belt}</Text>
             <Text>Age : {props.student.Age}</Text>
             <Text>Team : {getTeamName(props.student.Team_ID)}  </Text>
+
+            {/* <Text>{JSON.stringify(practicesList)}</Text> */}
+            <Calendar
+                markedDates={okDates}
+            />
         </View>
     )
 }
