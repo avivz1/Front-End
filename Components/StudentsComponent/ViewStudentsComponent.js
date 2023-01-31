6909f9e52ac5df7fd05c9e2e1b015c2f8498ff90
avivz1 <avivzamir123@gmail.com>
2023-01-31 14:30:51 +0200

    erase comments in page

--- Components/StudentsComponent/ViewStudentsComponent.js
+++ Components/StudentsComponent/ViewStudentsComponent.js
@@ -107,49 +107,6 @@ export default function ViewStudentsComponent() {
 
     }
 
-    // const pickImage = async () => {
-    //     let result = await ImagePicker.launchImageLibraryAsync({
-    //         mediaTypes: ImagePicker.MediaTypeOptions.All,
-    //         allowsEditing: true,
-    //         aspect: [4, 3],
-    //         quality: 1,
-    //     });
-
-    //     if (!result.canceled) {
-    //         setPickedImage(result.assets[0].uri);
-    //         addOrUpdateStudentPhoto()
-    //     }
-    // };
-
-    // const verifyPermissions = async () => {
-    //     const result = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    //     // const result = await Permissions.askAsync(Permissions.CAMERA);
-    //     if (result.status !== 'granted') {
-    //         Alert.alert(
-    //             'Insufficient permissions!',
-    //             'You need to grant camera permissions to use this app.',
-    //             [{ text: 'Okay' }]
-    //         );
-    //         return false;
-    //     }
-    //     return true;
-    // };
-
-    // const takeImageHandler = async () => {
-    //     const hasPermission = await verifyPermissions();
-    //     if (!hasPermission) {
-    //         return;
-    //     }
-    //     const image = await ImagePicker.launchCameraAsync({
-    //         allowsEditing: true,
-    //         aspect: [16, 14],
-    //         quality: 0.5,
-
-    //     });
-
-    //     setPickedImage(image.assets[0].uri);
-    //     addOrUpdateStudentPhoto()
-    // };
 
     const StudentCardPress = (stu, btnType) => {
         setPickedStudent(stu);
@@ -157,12 +114,7 @@ export default function ViewStudentsComponent() {
             case 'updateRequest':
                 getAllStudents();
                 break;
-            // case 'pictureBtn': Alert.alert('a', 'b', [
-            //     { text: 'Cancel', style: 'cancel' },
-            //     { text: 'Take a Picture', onPress: () => { takeImageHandler() } },
-            //     { text: 'Gallery', onPress: () => { pickImage() } }
-            // ])
-            //     break;
+ 
             case 'detailsBtn': setDetailsVisible(true)
                 break;
             case 'editBtn': setEditVisible(true)
