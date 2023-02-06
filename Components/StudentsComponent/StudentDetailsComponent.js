e2904ce286739406e46e6f913471d0d6df6fb28b
avivz1 <avivzamir123@gmail.com>
2023-02-06 15:54:13 +0200

    adding activities to students. make emergency contact - pressable to the dialer

--- Components/StudentsComponent/StudentDetailsComponent.js
+++ Components/StudentsComponent/StudentDetailsComponent.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Button, TextInput, ViewComponent, Platform, ScrollView, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ViewComponent, Platform, ScrollView, Alert, Linking } from 'react-native';
 import { useContext, useState, useEffect } from 'react'
 import { Context } from '../../ContextAPI/Context';
 import axios from 'axios';
@@ -18,9 +18,10 @@ export default function StudentDetailsComponent(props) {
     const [attendanceArray, setAttendanceArray] = useState([])
     const [newActivityEvent, setNewActivityEvent] = useState('')
     const [newActivityNote, setNewActivityNote] = useState('')
-    const [activities, setActivities] = useState(props.student.Activities?props.student.Activities:[])
+    const [newActivityDate, setNewActivityDate] = useState('')
+    const [activities, setActivities] = useState(props.student.Activities ? props.student.Activities : [])
+    const [activityInputErrors, setActivityInputErrors] = useState('')
 
-    let x = []
 
     useEffect(() => {
         getStudentPracticesDetails()
@@ -31,16 +32,68 @@ export default function StudentDetailsComponent(props) {
         processDates();
     }, [attendanceArray])
 
+    const getAllActivities = () => {
+        axios.post('http://' + IP + '/students/getstudentactivities', { userId: userIdValue, stuId: props.student._id }).then(res => {
+            if (res.data) {
+                setActivities(res.data)
+            } else {
+                Alert.alert('failed to get activities')
+            }
+        })
+    }
+
+    const isInputValid = () => {
+        let arr = []
+        if (newActivityEvent == '' || newActivityEvent == undefined) {
+            arr.push('activityEventError')
+        }
+        if (newActivityNote == '' || newActivityNote == undefined) {
+            arr.push('activityNoteError')
+        }
+        if (newActivityDate == '' || newActivityDate == undefined) {
+            arr.push('activityDateError')
+        }
+        if (arr.length == 0) {
+            setActivityInputErrors([])
+            console.log(true)
+            return true;
+        } else {
+            setActivityInputErrors(arr)
+            console.log(false)
+            return false;
+        }
+    }
+
     const addNewActivity = () => {
-        axios.post('http://' + IP + '/students/addnewstudentactivity', { userId: userIdValue, stuId: props.student._id, activity: { Event: newActivityEvent, Note: newActivityNote } })
-            .then(res => {
-                if (res) {
-                    setActivities((activityList) => [ ...activityList,{ Event: newActivityEvent, Note: newActivityNote }])
-                    Alert.alert('Success')
-                } else {
-                    console.log('Somthing went wrong')
-                }
-            })
+        if (isInputValid) {
+            axios.post('http://' + IP + '/students/addnewstudentactivity', { userId: userIdValue, stuId: props.student._id, activity: { Event: newActivityEvent, Note: newActivityNote, Date: newActivityDate } })
+                .then(res => {
+                    if (res) {
+                        getAllActivities()
+                        // setActivities((activityList) => [...activityList, { Event: newActivityEvent, Note: newActivityNote, Date: newActivityDate }])
+                        Alert.alert('Success')
+                    } else {
+                        console.log('Somthing went wrong')
+                    }
+                })
+        } else {
+            Alert.alert('empty activity details')
+        }
+    }
+
+    const deleteActivity = (act) => {
+        axios.post('http://' + IP + '/students/deleteactivity', { activity: act, userId: userIdValue, stuId: props.student._id }).then(res => {
+            if (res.data) {
+                // let updateActivities = activities.filter(activity => activity._id != act._id)
+
+                getAllActivities()
+
+                // setActivities(updateActivities)
+                //delete data from activities state
+            } else {
+                Alert.alert('Error')
+            }
+        })
     }
 
     const getStudentPracticesDetails = () => {
@@ -110,8 +163,20 @@ export default function StudentDetailsComponent(props) {
         return name;
     }
 
-    const onRowLongPress = () => {
-        console.log('onLongPress')
+    const onRowLongPress = (data) => {
+        Alert.alert('Delete This Activity?', data.Event, [
+            { text: 'Cancel' },
+            {
+                text: 'Ok', onPress: () => {
+                    deleteActivity(data)
+                }
+            }
+        ])
+    }
+
+
+    const openDialer = () => {
+        Linking.openURL(`tel:0${props.student.EmergencyContact.Phone}`);
     }
 
 
@@ -119,14 +184,15 @@ export default function StudentDetailsComponent(props) {
 
         <View style={{ height: '80%' }}>
             <Text>Name : {props.student.Name}</Text>
-            <Text>Phone : {props.student.Phone}</Text>
+            <Text>Phone :0{props.student.Phone}</Text>
             <Text>Belt : {props.student.Belt}</Text>
             <Text>Age : {props.student.Age}</Text>
             <Text>Team : {getTeamName(props.student.Team_ID)}  </Text>
             <Text>Precentage By Month :{presentMonthPrecentage ? presentMonthPrecentage.toFixed(2) : 0}%  </Text>
             <Text>City : {props.student.City}</Text>
-            <Text>Emergency Contact : {props.student.EmergencyContact.Name + '  ' + props.student.EmergencyContact.Phone}</Text>
-
+            {/* <Text>Emergency Contact : {props.student.EmergencyContact.Name + '  ' + props.student.EmergencyContact.Phone}</Text> */}
+            <Text> Emergency Contact :</Text>
+            <Button title='Dial Emergency' onPress={openDialer} />
             <ScrollView style={{ height: '50%' }}>
                 <Calendar
                     onMonthChange={monthChange}
@@ -137,17 +203,17 @@ export default function StudentDetailsComponent(props) {
 
                     <DataTable.Header style={{ backgroundColor: '#ADD8E6', fontWeight: 'bold' }}>
                         <DataTable.Title style={{ fontWeight: 'bold' }}>Event</DataTable.Title>
-                        <DataTable.Title style={{ fontWeight: 'bold' }}>Notes</DataTable.Title>
+                        <DataTable.Title style={{ fontWeight: 'bold' }}>Note</DataTable.Title>
+                        <DataTable.Title style={{ fontWeight: 'bold' }}>Date</DataTable.Title>
                     </DataTable.Header>
 
                     <ScrollView>
                         {activities.length > 0 ? activities.map((act, index) => {
-                            console.log(act)
-                            console.log('--------------------------------------------------')
                             return (
-                                <DataTable.Row key={index} onLongPress={() => onRowLongPress()}>
+                                <DataTable.Row key={act._id} onLongPress={() => onRowLongPress(act)} onPress={() => { }}>
                                     <DataTable.Cell>{act.Event}</DataTable.Cell>
                                     <DataTable.Cell>{act.Note}</DataTable.Cell>
+                                    <DataTable.Cell>{act.Date}</DataTable.Cell>
                                 </DataTable.Row>
                             )
                         })
@@ -158,11 +224,19 @@ export default function StudentDetailsComponent(props) {
 
                 </DataTable>
                 <View style={{ margin: 30 }}>
-                    <Text>Add Activity </Text>
-                    <Text>Add Event </Text>
+                    <Text style={[styles.mainHeadLines]}>Add Activity </Text>
+                    <Text style={[styles.smallHeadLines]}>Add Event </Text>
                     <TextInput placeholder='New Activity Event' onChangeText={(event) => setNewActivityEvent(event)} />
-                    <Text>Add Note </Text>
+                    {activityInputErrors.includes('activityEventError') && <Text>This is required</Text>}
+
+                    <Text style={[styles.smallHeadLines]}>Add Note </Text>
                     <TextInput placeholder='New Activity Note' onChangeText={(note) => setNewActivityNote(note)} />
+                    {activityInputErrors.includes('activityNoteError') && <Text>This is required</Text>}
+
+                    <Text style={[styles.smallHeadLines]}>Date </Text>
+                    <TextInput placeholder='New Activity Date' onChangeText={(date) => setNewActivityDate(date)} />
+                    {activityInputErrors.includes('activityDateError') && <Text>This is required</Text>}
+
                     <Button title='Add new Activity' onPress={() => addNewActivity()} />
                 </View>
             </ScrollView>
@@ -176,6 +250,12 @@ const styles = StyleSheet.create({
     container: {
         paddingTop: Platform.OS === 'android' ? 80 : 0,
         flex: 1,
-
+    },
+    smallHeadLines: {
+        fontWeight: 'bold'
+    },
+    mainHeadLines: {
+        fontWeight: 'bold',
+        margin:10
     }
 });
